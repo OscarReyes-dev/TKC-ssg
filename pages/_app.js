2050e1dd4aed8a48a78561c587b5b6f9420fcb27
OscarReyes-dev <oscar.rdev@gmail.com>
2021-11-12 16:51:12 -0800

    Incorporated the script component to the app js file

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import '../styles/reset.css'
 import Head from "next/head";
 
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
 return (
@@ -29,11 +30,11 @@ return (
 
 
                   {/* <!-- BOOTSTRAP 5 --> */}
-                  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+                  <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
                   integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-                  crossorigin="anonymous"></script>
+                  crossOrigin="anonymous"></Script>
                   {/* <!-- JQUERY --> */}
-                  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+                  <Script src="https://code.jquery.com/jquery-3.6.0.min.js"></Script>
 
 
 
